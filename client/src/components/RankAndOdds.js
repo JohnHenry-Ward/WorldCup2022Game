0bfee9ea6ecec2285a88cb25ad648156e57f9c25
JohnHenry-Ward <johnhenry514@gmail.com>
2022-11-15 14:42:06 -0800

    format rank and odds

--- client/src/components/RankAndOdds.js
+++ client/src/components/RankAndOdds.js
@@ -5,10 +5,10 @@ const RankAndOdds = ({ team }) => {
 
     return (
         <div className='ranksAndOddsBoard-team'>
-            <img className="team-flag" src={require("../images/flags/"+team.code.toUpperCase()+".png")} alt={team.code+' flag'} />
-            <div>{team.country}</div>
-            <div>Rank: {team.rank}</div>
-            <div>Odds: {team.odds}</div>
+            <img id='ranksAndOddsBoard-flag' className="team-flag" src={require("../images/flags/"+team.code.toUpperCase()+".png")} alt={team.code+' flag'} />
+            <div id='ranksAndOddsBoard-country'>{team.country}</div>
+            <div id='ranksAndOddsBoard-rank'>Rank: {team.rank}</div>
+            <div id='ranksAndOddsBoard-odds'>Odds: {team.odds}</div>
         </div>
     );
 }
