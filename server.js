e96cd8aaef4ed5fa4f46ed03f911c9674d04654f
JohnHenry-Ward <johnhenry514@gmail.com>
2022-11-15 15:42:34 -0800

    remove CSP

--- server.js
+++ server.js
@@ -20,13 +20,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 /* Middleware */
-app.use((req, res, next) => {
-    res.setHeader(
-        'Content-Security-Policy',
-        "default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob:;"
-    );
-    next();
-});
 app.use(bodyParser.urlencoded({ extended: false }));
 
 /* Routes */
