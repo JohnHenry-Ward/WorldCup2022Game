6ee1a4f04d6bc3aacfc814194c6937ec853863a2
JohnHenry-Ward <johnhenry514@gmail.com>
2022-11-15 15:35:29 -0800

    update CSP

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use((req, res, next) => {
     res.setHeader(
         'Content-Security-Policy',
-        "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';"
+        "default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline'; frame-ancestors * data: blob:;"
     );
     next();
 });
